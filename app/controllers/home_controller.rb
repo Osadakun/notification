a41c8f8fc8af077823bb9559a4df88adc687e552
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-23 21:54:49 +0900

    なんかミスったような気がする

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   def top
     @member = Member.all.order(in_time: :asc)
-    @cnt = Count.find(1)
-    @cnt.save
   end
 
   def show
@@ -30,18 +28,12 @@ class HomeController < ApplicationController
     @times = Time.now
     @member = Member.new(name: params[:name], in_time: @times, out_time: params[:out_time])
     @member.save
-    @cnt = Count.find(1)
-    @cnt.counter += 1
-    @cnt.save
     redirect_to('/')
   end
 
   def create_res
     @member = Member.new(name: params[:name], in_time: params[:in_time], out_time: params[:out_time])
     @member.save
-    @cnt = Count.find(1)
-    @cnt.counter += 1
-    @cnt.save
     redirect_to('/')
   end
 end
