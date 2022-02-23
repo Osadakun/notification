7432cb2527c069176bdd068cfa51c07bb120b019
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-23 23:06:50 +0900

    なんかまじで原因不明

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module LabNotification
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.time_zone = 'Tokyo'
+    config.active_record.time_zone_aware_types = [:datetime, :time]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
