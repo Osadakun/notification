21929b38b6c24ebdc6f130efd51cfd339fad95f8
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-23 21:38:07 +0900

    一旦セーブwo

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,47 @@
+class HomeController < ApplicationController
+  def top
+    @member = Member.all.order(in_time: :asc)
+    @cnt = Count.find(1)
+    @cnt.save
+  end
+
+  def show
+    @member = Member.find_by(id: params[:id])
+  end
+
+  def destroy
+    @member = Member.find_by(id: params[:id])
+    @member.destroy
+    @member.save
+    redirect_to('/')
+  end
+
+  def edit
+    @member = Member.find_by(id: params[:id])
+  end
+
+  def update
+    @member = Member.find_by(id: params[:id])
+    @member.update(name: params[:name], in_time: params[:in_time], out_time: params[:out_time])
+    redirect_to('/')
+  end
+
+  def create_enter
+    @times = Time.now
+    @member = Member.new(name: params[:name], in_time: @times, out_time: params[:out_time])
+    @member.save
+    @cnt = Count.find(1)
+    @cnt.counter += 1
+    @cnt.save
+    redirect_to('/')
+  end
+
+  def create_res
+    @member = Member.new(name: params[:name], in_time: params[:in_time], out_time: params[:out_time])
+    @member.save
+    @cnt = Count.find(1)
+    @cnt.counter += 1
+    @cnt.save
+    redirect_to('/')
+  end
+end
---
+++ db/migrate/20220223123150_create_members.rb
@@ -0,0 +1,12 @@
+class CreateMembers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :members do |t|
+      t.text :name
+      t.text :room
+      t.datetime :in_time
+      t.datetime :out_time
+
+      t.timestamps
+    end
+  end
+end
